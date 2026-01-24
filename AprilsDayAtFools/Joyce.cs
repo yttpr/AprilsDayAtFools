a0eea61b4fade4d65c830fec712fcb2bc5ad5ca0
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-01-24 11:30:58 -0800

    #

--- AprilsDayAtFools/Joyce.cs
+++ AprilsDayAtFools/Joyce.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace AprilsDayAtFools
 {
-    [BepInPlugin("AprilsFoolsDay.Joyce", "Aprils' Day at Fools", "1.3.4.4")]//next tweak would be 1.3.5
+    [BepInPlugin("AprilsFoolsDay.Joyce", "Aprils' Day at Fools", "1.3.4.5")]//next tweak would be 1.3.5
     [BepInDependency("BrutalOrchestra.BrutalAPI", BepInDependency.DependencyFlags.HardDependency)]
     public class Joyce : BaseUnityPlugin
     {
