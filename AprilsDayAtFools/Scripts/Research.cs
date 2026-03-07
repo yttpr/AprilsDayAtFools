d968a5df4d9a03c4306becc4af33f43980f83c43
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-06 20:46:54 -0800

    research

--- AprilsDayAtFools/Scripts/Research.cs
+++ AprilsDayAtFools/Scripts/Research.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
+using Tools;
 
 namespace AprilsDayAtFools
 {
@@ -17,6 +18,28 @@ namespace AprilsDayAtFools
             if (foundling == null || foundling.Equals(null)) foundling = LoadedAssetsHandler.GetEnemy("DerelictFoundling_EN");
 
             foreach (EffectInfo info in foundling.enterEffects) Debug.Log(info.effect);
+
+            CasterAddExtraAbilitySetFromPreviousEffect test = foundling.enterEffects[1].effect as CasterAddExtraAbilitySetFromPreviousEffect;
+            Debug.Log("going through ABILITY SETS");
+            foreach (IntListWrapper wrapper in test._AbilitySets)
+            {
+                string working = "";
+                foreach (int num in wrapper.list)
+                {
+                    working += num.ToString();
+                    working += ", ";
+                }
+                Debug.Log("int list wrapper: " + working);
+            }
+            Debug.Log("going through POOLS DATA");
+            foreach (ExtraAbilityInfoListWrapper wrapper in test._PoolsData)
+            {
+                Debug.Log("NEW POOL DATA");
+                foreach (ExtraAbilityInfo info in wrapper.list)
+                {
+                    Debug.Log(info.ability);
+                }
+            }
         }
     }
 }
