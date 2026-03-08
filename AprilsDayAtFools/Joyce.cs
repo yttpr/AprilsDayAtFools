c739357a2872ea0d869656d57b74e46b6b3b33a9
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-07 16:56:44 -0800

    suicide command test

--- AprilsDayAtFools/Joyce.cs
+++ AprilsDayAtFools/Joyce.cs
@@ -67,6 +67,7 @@ namespace AprilsDayAtFools
             Class1.PCall(TargettedAlreadyHandler.Setup);
             Class1.PCall(BiggerCharacterDragSizeHook.Setup);
             Class1.PCall(Research.Add);
+            Class1.PCall(KillCommand.Add);
 
             Class1.PCall(Anesthetics.Add);
             Class1.PCall(Power.Add);
---
+++ AprilsDayAtFools/Scripts/KillCommand.cs
@@ -0,0 +1,32 @@
+﻿using BrutalAPI;
+using FMODUnity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprilsDayAtFools
+{
+
+    public static class KillCommand
+    {
+        public static DebugCommand KILL;
+        public static void Add()
+        {
+            KILL = new DebugCommand("die", "Instantly die.", new List<DebugCommandArgument>(), delegate
+            {
+                CombatManager.Instance.AddPriorityRootAction(new PerformDelegateAction(delegate (CombatStats x)
+                {
+                    foreach (CharacterCombat chara in CombatManager.Instance._stats.CharactersOnField.Values)
+                    {
+                        chara.Damage(999, null, "Basic");
+                    }
+                }));
+                DebugController.Instance.WriteLine("Killing self.");
+            });
+
+
+            DebugController.Commands.children.Add(KILL);
+        }
+
+    }
+}
