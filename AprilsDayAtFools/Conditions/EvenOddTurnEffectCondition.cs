25dd8b396114f18fb87db28f9a3f56ed412f2a55
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-08 11:35:41 -0700

    ok. needlse rework/total buff actually.

---
+++ AprilsDayAtFools/Conditions/EvenOddTurnEffectCondition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace AprilsDayAtFools
+{
+    public class EvenOddTurnEffectCondition : EffectConditionSO
+    {
+        public bool Odd;
+        public override bool MeetCondition(IUnit caster, EffectInfo[] effects, int currentIndex)
+        {
+            return (CombatManager.Instance._stats.TurnsPassed + 1) % 2 > 0 == Odd;
+        }
+
+        public static EvenOddTurnEffectCondition Create(bool odd)
+        {
+            EvenOddTurnEffectCondition ret = ScriptableObject.CreateInstance<EvenOddTurnEffectCondition>();
+            ret.Odd = odd;
+            return ret;
+        }
+    }
+}
---
+++ AprilsDayAtFools/Effects/DelayedAttackDamageByExitEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprilsDayAtFools
+{
+    public class DelayedAttackDamageByExitEffect : MaskedAddDelayedAttackEffect
+    {
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            return base.PerformEffect(stats, caster, targets, areTargetSlots, entryVariable * PreviousExitValue, out exitAmount);
+        }
+    }
+}
