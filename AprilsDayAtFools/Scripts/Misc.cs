d0cb966df3efb59cac006ae1f594e90e4af2532a
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-14 02:01:11 -0700

    defacer can show up on birthday

--- AprilsDayAtFools/Scripts/Misc.cs
+++ AprilsDayAtFools/Scripts/Misc.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace AprilsDayAtFools
@@ -6,10 +7,21 @@ namespace AprilsDayAtFools
     public static class April
     {
         public static string AppData => Application.persistentDataPath;
+        public static bool Birthday
+        {
+            get
+            {
+                //return true;
+                if (DateTime.Now.Month == 4 && DateTime.Now.Day == 1) return true;
+                return false;
+            }
+        }
         public static bool Me
         {
             get
             {
+                if (Birthday) return true;
+
                 bool ret = Directory.Exists(AppData + "/Mods/") && Directory.Exists(AppData + "/Mods/DayAtFools/") && File.Exists(AppData + "/Mods/DayAtFools/secret.txt");
 
                 return ret;
