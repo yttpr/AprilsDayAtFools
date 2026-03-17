21b50dc6792f74e2fa6e2d4714c978ec7dc73399
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-17 00:49:36 -0700

    work for the animations for beam

---
+++ AprilsDayAtFools/Effects/ReturnAbilityCountEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprilsDayAtFools
+{
+    public class ReturnAbilityCountEffect : EffectSO
+    {
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+
+            foreach (TargetSlotInfo target in targets)
+            {
+                if (target.HasUnit)
+                    exitAmount += target.Unit.AbilityCount;
+            }
+
+            return exitAmount > 0;
+        }
+    }
+}
