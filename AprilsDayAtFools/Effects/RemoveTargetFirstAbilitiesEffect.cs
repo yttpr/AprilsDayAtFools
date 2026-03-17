e529038dfc79b09875c950256a70cc9ebc628a17
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-17 00:02:42 -0700

    remove enemy abilities effect created

---
+++ AprilsDayAtFools/Effects/RemoveTargetFirstAbilitiesEffect.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace AprilsDayAtFools
+{
+    public class RemoveTargetFirstAbilitiesEffect : EffectSO
+    {
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+
+            foreach (TargetSlotInfo target in targets)
+            {
+                if (target.HasUnit)
+                {
+                    if (target.Unit is CharacterCombat chara)
+                    {
+                        for (int i = 0; i < entryVariable; i++)
+                        {
+                            if (chara.CombatAbilities.Count <= 0) continue;
+
+                            AbilitySO abil = chara.CombatAbilities[0].ability;
+                            chara.CombatAbilities.RemoveAt(0);
+
+                            CombatManager.Instance.AddUIAction(new DestroyAbilityUIAction(chara.ID, chara.IsUnitCharacter, abil._abilityName, abil.abilitySprite));
+
+                            exitAmount++;
+                        }
+
+                        CombatManager.Instance.AddUIAction(new CharacterUpdateAllAttacksUIAction(chara.ID, chara.CombatAbilities.ToArray()));
+                    }//chara
+                    else if (target.Unit is EnemyCombat enemy)
+                    {
+                        //remove abilities
+                        for (int i = 0; i < entryVariable; i++)
+                        {
+                            if (enemy.Abilities.Count <= 0) continue;
+
+                            AbilitySO abil = enemy.Abilities[0].ability;
+                            enemy.Abilities.RemoveAt(0);
+
+                            CombatManager.Instance.AddUIAction(new DestroyAbilityUIAction(enemy.ID, enemy.IsUnitCharacter, abil._abilityName, abil.abilitySprite));
+
+                            exitAmount++;
+                        }
+
+                        //update timeline ids
+                        if (enemy.TurnsInTimeline <= 0) continue;
+                        List<int> turns_removed = [];
+
+                        for (int i = stats.timeline.CurrentTurn; i < stats.timeline.Round.Count; i++)
+                        {
+                            if (stats.timeline.Round[i].isPlayer) continue;
+
+                            if (stats.timeline.Round[i].turnUnit == enemy)
+                            {
+                                if (stats.timeline.Round[i].abilitySlot < entryVariable)
+                                {
+                                    stats.timeline.Round.RemoveAt(i);
+                                    enemy.TurnsInTimeline--;
+                                    turns_removed.Add(i);
+                                }
+                                else
+                                {
+                                    TurnInfo turn = stats.timeline.Round[i];
+                                    turn.abilitySlot -= entryVariable;
+                                    stats.timeline.Round[i] = turn;
+                                }
+                            }
+                        }
+
+                        CombatManager.Instance.AddUIAction(new RemoveSlotTimelineUIAction(turns_removed.ToArray()));
+                        CombatManager.Instance.AddUIAction(new UpdateTimelinePointerUIAction(stats.timeline.CurrentTurn));
+
+                        CombatManager.Instance.AddUIAction(new EnemyUpdateAllAttacksUIAction(enemy.ID, enemy.Abilities.ToArray()));
+                    }//enemy
+                }
+            }
+
+            return exitAmount > 0;
+        }
+    }
+
+
+    public class DestroyAbilityUIAction : CombatAction
+    {
+        public int _id;
+
+        public string _itemName;
+
+        public bool _isChara;
+
+        public Sprite _itemSprite;
+
+        public DestroyAbilityUIAction(int id, bool isChara, string name, Sprite sprite = null)
+        {
+            _id = id;
+            _isChara = isChara;
+            _itemName = name;
+            _itemSprite = sprite;
+        }
+
+        public override IEnumerator Execute(CombatStats stats)
+        {
+            yield return stats.combatUI.ShowPassiveInformation(_id, _isChara, _itemName + " Removed", stats.audioController.itemConsumed, _itemSprite);
+        }
+    }
+}
