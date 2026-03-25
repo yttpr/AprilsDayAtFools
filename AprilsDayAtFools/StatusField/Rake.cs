88766a64c22a92adab6fa89e80e181b5532afbc4
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-24 20:20:01 -0700

    finalizd rake stuff

--- AprilsDayAtFools/StatusField/Rake.cs
+++ AprilsDayAtFools/StatusField/Rake.cs
@@ -93,4 +93,12 @@ namespace AprilsDayAtFools
             
         }
     }
+    public class ApplyRakeEffect : FieldEffect_Apply_Effect
+    {
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            _Field = Rake.Object;
+            return base.PerformEffect(stats, caster, targets, areTargetSlots, entryVariable, out exitAmount);
+        }
+    }
 }
