88595d928768d0290dfcbd0a4dff6ea6e66c2ee5
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-04-10 18:53:04 -0700

    reworked beheading to be. hopefully good? i think its an interesting idea.

---
+++ AprilsDayAtFools/Conditions/FirstDamageUpItemCondition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AprilsDayAtFools
+{
+    public class FirstDamageUpItemCondition : EffectorConditionSO
+    {
+        public string ID;
+        public override bool MeetCondition(IEffectorChecks effector, object args)
+        {
+            if (args is DamageDealtValueChangeException exception && effector is IUnit unit)
+            {
+                if (unit.SimpleGetStoredValue(ID) <= 0)
+                {
+                    unit.SimpleSetStoredValue(ID, 1);
+                    unit.ShowItem();
+                    exception.AddModifier(new AdditionValueModifier(true, 7));
+                }
+            }
+            return false;
+        }
+    }
+}
