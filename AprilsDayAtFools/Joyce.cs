e5e7d40f30d5f668341fe8fe5c161033c5fc140e
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-04-10 18:35:29 -0700

    # change. we re reworking the alpha move again...

--- AprilsDayAtFools/Joyce.cs
+++ AprilsDayAtFools/Joyce.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace AprilsDayAtFools
 {
-    [BepInPlugin("AprilsFoolsDay.Joyce", "Aprils' Day at Fools", "1.4.7")]//next tweak would be 1.4.8
+    [BepInPlugin("AprilsFoolsDay.Joyce", "Aprils' Day at Fools", "1.4.8")]//next tweak would be 1.4.8
     [BepInDependency("BrutalOrchestra.BrutalAPI", BepInDependency.DependencyFlags.HardDependency)]
     public class Joyce : BaseUnityPlugin
     {
